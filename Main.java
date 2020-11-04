87f782c55a71d5d719e914c0c710a1c9cc3f42fe
adamlodeiro <adam.lodeiro@patelhs.org>
2020-11-04 19:06:27 +0000

    Fixed the order of my code

--- Main.java
+++ Main.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-  int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-  
-  String[] names = new String[15];
-  
-  for (int i=0; i<16; i++ ){
-    System.out.println( names[i] );
-  }
-  Scanner scan = new Scanner(System.in);
-    System.out.println("What are you're guys names?");
-  String name = scan.next();
+    int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    String[] names = new String[15];
+    Scanner scan = new Scanner(System.in);
 
-    System.out.println(name);
+    for (int i = 0; i < 15; i++) {
+      System.out.println("What are you're guys names?");
+      names[1]  = scan.next();
+    }
+
   }
 }
